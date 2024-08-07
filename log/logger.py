b2054c75fb378cb6511a0f8e408608e0c80a910f
Ihor Tresnystkyi <ihortresnystkyi@Ihors-MacBook-Pro.local>
2024-08-07 23:14:30 +0100

    Updated logger.py

--- log/logger.py
+++ log/logger.py
@@ -1,12 +1,7 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+
 logger = logging.getLogger(__name__)
-
-
-file_handler = logging.FileHandler(filename='../discord.log', encoding='utf-8', mode='w')
-file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s'))
-logger.addHandler(file_handler)
-
 
 logger.info("Starting the bot...")
